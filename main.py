35766631792de5d507215ada648b2582f40a60f8
Alejandro.DGZ <3NJDEGUZMAN@gmail.com>
2025-02-08 11:56:43 +0000

    testing database connection

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
-import streamlit
-import pymongo
+# import streamlit
 
-print("hello world")
+connectionString = "mongodb+srv://dbAdmin:Password1234@contingency.xxt06.mongodb.net/?retryWrites=true&w=majority&appName=contingency"
+
+# streamlit.write("Hello World")
+from pymongo import MongoClient
+
+try:
+    client = MongoClient(
+        connectionString
+    )  # Change this if using a different host
+    db = client.test  # Test connection with a sample database
+    print("Connection successful!")
+except Exception as e:
+    print("Connection failed:", e)
